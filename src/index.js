e597695c635d09eb4063054751f193bbeaba2f7d
Derek Lam <dc.lam@mail.utoronto.ca>
2018-11-12 03:14:55 -0500

    Start rendering fins on multiple canvases
    Prepare for particle integration and merging with existing lam.io CSS
    work

--- src/index.js
+++ src/index.js
@@ -2,33 +2,114 @@ import {logo, bvrd, simplex, line} from './Assets/Meshes';
 import XY2M from './Util/XY2M';
 import Q from 'q';
 import {DEFAULT_STROKE_STYLE} from './Geometry.js';
+import {exp_rv} from './Util/math.js';
+
+import Proton from 'proton-js';
 
 const P_SCREEN = $V([0, 0, 4]);
 const P0 = $V([0, 0, 15]);
 const d_0_SCREEN = P_SCREEN.subtract(P0);
 
+const SAFETY_MARGIN = 0.02;
+
 const paths = logo.shard(P0, P_SCREEN);
 const pixel_ratio = window.devicePixelRatio || 1;
 
-function on_load_resize_dirty_flag() {
-	const canvas = document.getElementById('hero_canvas');
-	const rect = canvas.getBoundingClientRect();
-	canvas.width = rect.width * pixel_ratio;
-	canvas.height = rect.height * pixel_ratio;
-}
-
 const art_scale_from_width = w => Math.max(w, 0.3*w + 500)
 
-window.addEventListener('load', () => {
-	const canvas = document.getElementById('hero_canvas');
-	const ctx = canvas.getContext('2d', { alpha: false });
+const [F_top_fins, F_bottom_fins] = [[[1, 2, 3, 4], idx => `img/graphics/top-fins/${idx}.png`], [[1, 2, 3, 4, 5, 6, 7, 8], idx => `img/graphics/bottom-fins/${idx}.png`]].map(([idxs, f_namer]) => Q.all(idxs.map(idx => {
+	const e = new Image();
+	const D = Q.defer();
+	e.src = f_namer(idx);
+	e.onload = () => D.resolve(e);
+	return D.promise;
+})));
+
+const F_bg = Q.all(['img/graphics/pattern-0.25.png', 'img/graphics/pattern-b.png'].map(src => {
+	const e = new Image();
+	const D = Q.defer();
+	e.src = src;
+	e.onload = () => {
+		const temp_canvas = document.createElement('canvas'),
+		      temp_ctx = temp_canvas.getContext('2d');
+		
+		temp_canvas.width = 12 * pixel_ratio;
+		temp_canvas.height = 12 * pixel_ratio;
+		temp_ctx.drawImage(e, 0, 0, temp_canvas.width, temp_canvas.height);
+		
+		D.resolve(temp_canvas);
+	};
+	return D.promise;
+}));
+
+function on_load_resize() {
+	const fullwidth_canvases = document.getElementsByClassName('fullwidth-canvas');
+	for(const canvas of fullwidth_canvases) {
+		const rect = canvas.getBoundingClientRect();
+		canvas.width = rect.width * pixel_ratio;
+	}
+	const fullheight_canvases = document.getElementsByClassName('fullheight-canvas');
+	for(const canvas of fullheight_canvases) {
+		const rect = canvas.getBoundingClientRect();
+		canvas.height = rect.height * pixel_ratio;
+	}
 	
-	let resize_dirty_flag = true;
-	on_load_resize_dirty_flag();
-	window.addEventListener('resize_dirty_flag', () => {
-		on_load_resize_dirty_flag();
-		resize_dirty_flag = true;
-	});
+	Q.all([F_top_fins, F_bottom_fins]).then(([h_top_fins, h_bottom_fins]) => {
+		const fin_canvas_collection = document.getElementsByClassName('fin-canvas');
+		const remainder_fin_canvas = document.getElementById('remainder_fin_canvas');
+		for(let fin_idx = 0; fin_idx < Math.max(fin_canvas_collection.length, h_top_fins.length, h_bottom_fins.length); fin_idx++) {
+			// fins that occlude particles
+			const h_top_fin = h_top_fins[fin_idx];
+			const h_bottom_fin = h_bottom_fins[fin_idx];
+			const fin_canvas = fin_canvas_collection[fin_idx] || remainder_fin_canvas;
+			const fin_ctx = fin_canvas.getContext('2d');
+			
+			const bbox = fin_canvas.getBoundingClientRect();
+			const SCALE = art_scale_from_width(bbox.width) * pixel_ratio;
+			const top_fin_ground = [467/1280, 319/1280]; // width-relative pixels, stolen from waypoints for logo sharding black box
+			if(h_top_fin != null) {
+				const aspect_ratio = h_top_fin.width / h_top_fin.height;
+				const true_height = (1 + SAFETY_MARGIN) * top_fin_ground[1] * SCALE;
+				fin_ctx.drawImage(h_top_fin, top_fin_ground[0] * SCALE, top_fin_ground[1] * SCALE - true_height, true_height * aspect_ratio, true_height);
+			}
+			
+			if(h_bottom_fin != null) {
+				const aspect_ratio = h_bottom_fin.width / h_bottom_fin.height;
+				const true_width = (1 + SAFETY_MARGIN) * SCALE;
+				fin_ctx.drawImage(h_bottom_fin, 0, bbox.height * pixel_ratio - true_width / aspect_ratio, true_width, true_width / aspect_ratio);
+				// debugger;
+			}
+		}
+	}, e => { debugger; }).catch(e => { debugger; });
+	
+	// Q.all([])
+}
+
+let num_resize_events = 0; // to track resize dirtiness in each state machine
+window.addEventListener('resize', () => {
+	on_load_resize();
+	num_resize_events++;
+});
+
+// window.addEventListener('load', () => {
+// 	const PARTICLE_RATE = 0.4; // 5/sec
+// 	const PARTICLE_SPAWN = {
+// 		O: [467.0227 / 1280, 319.434 / 1280],
+// 		R: 0.3912,
+// 		theta: Math.PI / 6,
+// 		delta_theta: Math.PI * 9 / 180
+// 	}; // describes an arc starting at O with radius R starting at theta sweeping delta_theta in which we spawn particles within the top fins
+	
+	
+// 	}).catch(e => {
+// 		debugger;
+// 	});
+// });
+window.addEventListener('load', () => {
+	const canvas = document.getElementById('shard_canvas');
+	const ctx = canvas.getContext('2d', { alpha: true });
+	
+	on_load_resize();
 
 	// state machine for interactivity
 	let is_interactive = false;
@@ -58,31 +139,8 @@ window.addEventListener('load', () => {
 	const PX_PER_PT = 30 * pixel_ratio;
 	const DELTA_THRESH = 0.0001; // steady-state
 	
-	const F_bg = Q.all(['img/graphics/pattern-0.25.png', 'img/graphics/pattern-b.png'].map(src => {
-		const e = new Image();
-		const D = Q.defer();
-		e.src = src;
-		e.onload = () => {
-			const temp_canvas = document.createElement('canvas'),
-			      temp_ctx = temp_canvas.getContext('2d');
-			
-			temp_canvas.width = 12 * pixel_ratio;
-			temp_canvas.height = 12 * pixel_ratio;
-			temp_ctx.drawImage(e, 0, 0, temp_canvas.width, temp_canvas.height);
-			
-			D.resolve(temp_canvas);
-		};
-		return D.promise;
-	}));
-	const F_fins = Q.all([1, 2, 3, 4].map(idx => {
-		const e = new Image();
-		const D = Q.defer();
-		e.src = `img/graphics/fins/${idx}.png`;
-		e.onload = () => D.resolve(e);
-		return D.promise;
-	}));
-	
-	Q.all([F_bg, F_fins]).then(([[ h_pattern, h_pattern_b ], h_fins]) => {
+	// SHARDER RENDERER
+	Q.all([F_bg]).then(([[ h_pattern, h_pattern_b ]]) => {
 		const make_closed_poly = (ctx, waypoints) => {
 			ctx.beginPath();
 			ctx.moveTo(waypoints[0][0], waypoints[0][1]);
@@ -102,21 +160,13 @@ window.addEventListener('load', () => {
 		}
 		({
 	 		current: $V([ (Math.random() - 0.5) * ANGULAR_RANGE.e(1), (Math.random() - 0.5) * ANGULAR_RANGE.e(2) ]),
+	 		num_resize_acks: -1,
 	 		draw: function(t) {
 	 			window.requestAnimationFrame(this.draw.bind(this));
 					
 	 			const SCALE = art_scale_from_width(canvas.getBoundingClientRect().width) * pixel_ratio; // [0, 1] -> true pixels
-	 			const SAFETY_MARGIN = 0.02;
 	 			
-	 			const fin_ground = [467/1280, 319/1280]; // width-relative pixels
-	 			for(const h_fin of h_fins) {
-	 				const aspect_ratio = h_fin.width / h_fin.height;
-	 				const true_height = (1 + SAFETY_MARGIN) * fin_ground[1] * SCALE;
-	 				ctx.drawImage(h_fin, fin_ground[0] * SCALE, fin_ground[1] * SCALE - true_height, true_height * aspect_ratio, true_height);
-	 			}
-	 			
-	 			
-	 			if(resize_dirty_flag) {
+	 			if(this.num_resize_acks !== num_resize_events) {
 		 			ctx.save();
 		 			// CCW clipping masks
 		 			(() => {
@@ -137,7 +187,7 @@ window.addEventListener('load', () => {
 	 			
 	 			
 	 			const delta = target.subtract(this.current);
-	 			if(delta.modulus() > DELTA_THRESH || resize_dirty_flag) {
+	 			if(delta.modulus() > DELTA_THRESH || this.num_resize_acks !== num_resize_events) {
 		 			ctx.save();
 		 			(() => {
 			 			const waypoints = [[74.1164 / 1280, 285.9054 / 1280], [74.1164 / 1280, 220.52 / 1280], [241.813 / 1280, 123.0864 / 1280], [467.0227 / 1280, 254.57 / 1280], [467.0227 / 1280, 319.434 / 1280], [299.3568 / 1280, 416.87 / 1280]]
@@ -190,10 +240,10 @@ window.addEventListener('load', () => {
 		 			ctx.restore();
 	 			}
 		 		
-		 		resize_dirty_flag = false;
+		 		this.num_resize_acks = num_resize_events;
 	 		}
 	 	}).draw(performance.now());
-	 });
+	 }, e => { debugger; }).catch(e => { debugger; });
 }); // new Polyline([$V([-0.5, -0.5, 0]), $V([0.5, -0.5, 0]), $V([0, 0.5, 0])])
 
 // new GeometryCollection([new Polyline([$V([-0.5, -0.5, 0]), $V([0.5, -0.5, 0]), $V([0, 0.5, 0])])])
